3cf88cb81645dea85f16e2be0acd36cf22df801d
Eric Della <ericdella13@gmail.com>
2021-02-16 16:58:50 -0800

    middleware functions

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,67 @@
-function logger(req, res, next) {
-  // DO YOUR MAGIC
+const users = require("../users/users-model")
+
+function logger(format) {
+  return (req, res, next) => {
+    const time = new Date().toISOString()
+
+    if (format === "short"){
+      console.log(`${time} ${req.method} ${req.url}`)
+    }
+    if (format === "long"){
+      console.log(`${time} ${req.ip}`)
+    }
+    next()
+  }
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+function validateUserId() {
+  return (req, res, next) => {
+    users.getById(req.params.id)
+    .then((user) => {
+      if (user) {
+        req.user = user
+        next()
+      } else {
+        res.status(404).json({
+          message: "User not Found"
+        })
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({
+        message: "Error retrieving user"
+      })
+    })
+  }
 }
 
-function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+function validateUser() {
+  return (req, res, next) => {
+    if (!req.body.name){
+      return res.status(400).json({
+        message: "Missing user data"
+      })
+    }
+    next()
+  }
 }
 
-function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+function validatePost() {
+  return (req, res, next) => {
+    if (!req.body.text){
+      return res.status(400).json({
+        message: "missing required text field"
+      })
+    }
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const users = require("./users-model")
 const router = express.Router();
 
 router.get('/', (req, res) => {
