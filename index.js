2d36d8ed06258052ebe89ac0f1699bdc1af791e4
Eric Della <ericdella13@gmail.com>
2021-02-19 10:49:30 -0800

    trying some reconfig

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 // require your server and launch it
+const express = require('express');
 
-const server = require("./api/server")
+const postsRouter = require("./api/posts/posts-router")
+const usersRouter = require("./api/users/users-router")
+const {logger} = require("./api/middleware/middleware")
+
+const server = express();
+
+// remember express by default cannot parse JSON in request bodies
+
+// global middlewares and routes need to be connected here
+
+server.use(express.json())
+server.use(logger("short"))
+
+
+server.use("/users", usersRouter)
+server.use("/posts", postsRouter)
+server.get('/', (req, res) => {
+  res.send(`<h2>Unit 4 Project 3</h2>`);
+});
+server.use((err, req, res) => {
+  console.log(err)
+
+  res.status(500).json({
+    message: "Something went wrong"
+  })
+})
 
 const port = process.env.PORT || 4000
 
